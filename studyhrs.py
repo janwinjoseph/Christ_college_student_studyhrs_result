a82f7c09d567a7a98780b296cad8bef59311b976
janwinjoseph <162668108+janwinjoseph@users.noreply.github.com>
2026-09-17 10:34:21 +0530

    Update studyhrs.py

--- studyhrs.py
+++ studyhrs.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-model=joblib.load("Logistic_regression_studnet_studyhors_model.pkl")
+model=joblib.load("Logistic_regression_student_studyhours_model.pkl")
 st.title("Student Pass/Fail based on Study Hours")
 hours=st.number_input("Enter Study Hours",min_value=0.0,max_value=15.0,value=5.0)
 if st.button("Predict"):
