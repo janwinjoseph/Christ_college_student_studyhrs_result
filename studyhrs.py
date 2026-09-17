4b3cb9024101e1ae4001c58459055b37addc593f
janwinjoseph <162668108+janwinjoseph@users.noreply.github.com>
2026-09-17 10:25:31 +0530

    Create studyhrs.py

---
+++ studyhrs.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import joblib
+model=joblib.("logisticregression.pkl")
+st.title("Student Pass/Fail based on Study Hours")
+hours=st.number_input("Enter Study Hours",min_value=0.0,max_value=15.0,value=5.0)
+if st.button("Predict"):
+  prediction=model.predict([[hours]])
+  if prediction[0]==1:
+    st.success("Student will PASS")
+  else:
+    st.error("Student will FAIL")
